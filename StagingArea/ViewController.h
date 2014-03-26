ff4d3280ebf5b973345141d6b3127800989f317f
robbykraft <robbykraft@gmail.com>
2014-03-26 00:23:55 -0700

    Initial Commit

---
+++ StagingArea/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StagingArea
+//
+//  Created by Robby Kraft on 3/26/14.
+//  Copyright (c) 2014 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StagingArea/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StagingArea
+//
+//  Created by Robby Kraft on 3/26/14.
+//  Copyright (c) 2014 Robby Kraft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
